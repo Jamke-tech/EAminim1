03ca0925f8dadca7bec6ffd377103e9c72785f0d
Jaume Tabernero <72195713+Jamke-tech@users.noreply.github.com>
2021-04-23 09:17:21 +0200

    Preparació del mínim 1 de Ea, creació de carpetes i directoris

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+
+
+
+
+//Creem un router per la nostra API
+
+const router = Router();
+
+
+//Rutes de la API
+//Exemple:
+/* router.route("/")
+    .post(Helloworld)*/
+
+
+
+
+export default router;
